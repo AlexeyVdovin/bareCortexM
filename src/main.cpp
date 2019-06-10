c35f2e29f9f02c31b119a063110b9687ba12610c
Alexey Vdovin <alexey.vdovin@aquantia.com>
2019-06-10 13:40:14 +0300

    Code Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -19,29 +19,6 @@
  *
  ******************************************************************************/
 
-// DO NOT INCLUDE THIS FILE ANYWHERE. THIS DEMO IS JUST A REFERENCE TO BE USED
-// IN YOUR MAIN SOURCE FILE.
-
-/* Reported GPS data:
-
-Node ID
-*GGA---
-  UTC Time
-  Latitude
-  Longitude
-  Position Fix Indicator
-  Satellites Used
-  HDOP
-  MSL Altitude
-  Geoid Separation
-*RMC---
-  Course over ground
-  Speed over ground
-  Magnetic variation
-  Date
-  Mode
-
- */
 #include <stdio.h>
 
 #include "clock.hpp"
@@ -51,31 +28,17 @@ Node ID
 #include "peripheral/afio.hpp"
 #include "peripheral/gpio.hpp"
 #include "peripheral/usart.hpp"
-#include "peripheral/can.hpp"
-
+#include "peripheral/adc.hpp"
+#include "peripheral/dma.hpp"
 #include "peripheral/tim.hpp"
 
 #define UART1_BAUD_RATE 115200
 
-#define CAN_TX  USB_HP_CAN_TX
-#define CAN_RX0 USB_LP_CAN_RX0
-#define CAN_RX1 CAN1_RX1
-#define CAN_SCE CAN1_SCE
-
-
 typedef PB6 U1TX;
 typedef PB7 U1RX;
-
-
 typedef PC13 LED;
-typedef PB8 CANRX;
-typedef PB9 CANTX;
 
 u64 tick = 0;
-volatile u32 rx0i  = 0;
-volatile u32 rx1i  = 0;
-volatile u32 txi   = 0;
-volatile u32 erri  = 0;
 
 void clk::hseFailureHandler()
 {
@@ -84,7 +47,7 @@ void clk::hseFailureHandler()
 
 void initializeGpio()
 {
-  // GPIOA::enableClock();
+  GPIOA::enableClock();
   GPIOB::enableClock();
   GPIOC::enableClock();
 
@@ -128,67 +91,6 @@ void initializeUsart1()
   U1RX::pullUp();
 }
 
-void initializeCan()
-{
-  AFIO::configureCan<afio::mapr::can::PB8_PB9>();
-
-  CAN::enableClocks();
-  CANTX::setMode(gpio::cr::AF_PUSH_PULL_2MHZ);
-  CANRX::setMode(gpio::cr::FLOATING_INPUT);
-
-  CAN::masterReset();
-
-  CAN::configEnter();
-  CAN::setBaudrate(500000);
-  CAN::configure(
-		  can::mcr::txfp::TX_QUEUE_PRIORITY,
-		  can::mcr::rflm::OVERRUN_NOT_LOCKED,
-		  can::mcr::nart::NO_RETRANSMIT,
-		  can::mcr::awum::AUTO_WAKEUP_ON,
-		  can::mcr::abom::AUTO_BUSOFF,
-		  can::mcr::ttcm::TIME_TRIGGERED_MODE_DISABLED,
-		  can::mcr::dbf::NORMAL_RUN,
-		  can::btr::lbkm::LOOPBACK_OFF, // LOOPBACK_ON / LOOPBACK_OFF
-		  can::btr::silm::NORMAL_OPERATION // SILENT_MODE / NORMAL_OPERATION
-		  );
-  CAN::txEmptyInterrupt<can::interrupt::ENABLE>();
-  CAN::rx0MsgInterrupt<can::interrupt::ENABLE>();
-  CAN::rx1MsgInterrupt<can::interrupt::ENABLE>();
-
-  CAN::unmaskInterrupts();
-
-  // Configure filters //
-  can::hdr ah, mh;
-
-  ah.addr = 0x00;
-  ah.ide = false; // std
-  ah.rtr = false;
-
-  mh.addr = 0; // 0x7ff;
-  mh.ide = true;
-  mh.rtr = true;
-
-  CAN::setFilterMaskBank<0, 0>(&ah, &mh);
-  CAN::setFilterActive<0>(true);
-
-  ah.addr = 0x00;
-  ah.ide = true; // ext
-  ah.rtr = false;
-
-  mh.addr = 0; // 0x1fffffff;
-  mh.ide = true;
-  mh.rtr = true;
-
-  CAN::setFilterMaskBank<1, 1>(&ah, &mh);
-  CAN::setFilterActive<1>(true);
-
-  // u8 d = CAN::getMode();
-  // printf("APB1 Frequency: %d\n", CAN::getFrequency());
-  // printf("------------ Mode: %d\n", d);
-
-  CAN::configExit();
-}
-
 void initializeTimer()
 {
   TIM2::enableClock();
@@ -200,109 +102,12 @@ void initializePeripherals()
   initializeGpio();
   initializeTimer();
   initializeUsart1();
-  initializeCan();
 
   TIM2::startCounter();
 }
 
-can::msg m0, m1;
-bool m0i = false;
-bool m1i = false;
-
 void loop()
 {
-  enum {
-	  IDLE = 0,
-	  KEY_LOCK,
-	  KEY_UNLOCK,
-	  KEY_RELEASE
-  };
-  static int s = IDLE, state = IDLE;
-  static u64 t0 = 0;
-  static u64 t1 = (u64)-1;
-  can::msg m;
-  u8 rc;
-
-  if(m0i)
-  {
-	  m = m0;
-	  m0i = false;
-	  printf("rs%c %04x %01x %08x %08x\n", m.h.rtr?'r':'w', m.h.addr, m.len, m.u[0], m.u[1]);
-	  if(1) {}
-	  // 0180  00 00 00 80  00 00 00 00 - KEY lock button
-	  else if(m.h.addr == 0x180 && m.len == 8 && m.u[0] == 0x80000000 && m.u[1] == 0x00000000) state = KEY_LOCK;
-	  // 0180  00 00 00 40  00 00 00 00 - KEY unlock button
-	  else if(m.h.addr == 0x180 && m.len == 8 && m.u[0] == 0x40000000 && m.u[1] == 0x00000000) state = KEY_UNLOCK;
-	  else if(m.h.addr == 0x180 && m.len == 8 && m.u[0] == 0x00000000 && m.u[1] == 0x00000000)
-	  {
-		  s = state;
-		  t1 = tick+200;
-	  }
-	  return;
-  }
-  if(m1i)
-  {
-	  m = m1;
-	  m1i = false;
-	  printf("rx%c %08x %01x %08x %08x\n", m1.h.rtr?'r':'w', m1.h.addr, m1.len, m1.u[0], m1.u[1]);
-  }
-
-  if(tick >= t0)
-  {
-    t0 = tick+1000;
-    printf("%d: tx: %d, rx0: %d, rx1: %d, err: %d\n", (int)(tick/1000), txi, rx0i, rx1i, erri);
-/*
-    m.h.addr = 0x1674A123;
-    m.h.ide = true;
-    m.h.rtr = false;
-    m.ts = 0;
-    m.fmi = 0;
-    m.len = 4;
-    m.u[0] = 0x5555aaaa;
-    m.u[1] = 0;
-
-    rc = CAN::Send(&m);
-    if(rc == 0xff)
-    {
-    	printf("Error can Tx!\n");
-    }
-    else
-    {
-    	u8 d = CAN::getMode();
-    	printf("Mode: %d, Tx dispatched to: %d\n", d, rc);
-    }
-*/
-    if(LED::isHigh()) { LED::setLow(); } else { LED::setHigh(); }
-  }
-
-  if(tick >= t1 && s != IDLE)
-  {
-    t1 = tick + 10;
-
-    m.h.addr = 0x180;
-    m.h.ide = false;
-    m.h.rtr = false;
-    m.ts = 0;
-    m.fmi = 0;
-    m.len = 8;
-
-    // 0180  55 00 00 00  00 00 00 00 - Driver door lock button
-    if(s == KEY_LOCK) m.u[0] = 0x00000055, m.u[1] = 0, s = KEY_RELEASE;
-    // 0180  aa 02 00 00  00 88 02 00 - Driver door unlock button
-    else if(s == KEY_UNLOCK)  m.u[0] = 0x000002aa, m.u[1] = 0x00028800, s = KEY_RELEASE;
-    else if(s == KEY_RELEASE) m.u[0] = 0, m.u[1] = 0, s = IDLE;
-
-    rc = CAN::Send(&m);
-    if(rc == 0xff)
-    {
-    	printf("Error can Tx!\n");
-    }
-    else
-    {
-    	u8 d = CAN::getMode();
-    	printf("M: %d, Tx %08x -> %d\n", d, m.u[0], rc);
-    }
-  }
 
 }
 
@@ -325,60 +130,3 @@ void interrupt::TIM2()
   ++tick;
 }
 
-void interrupt::CAN_TX()
-{
-	++txi;
-
-	can::mailbox::Complete m = CAN::getTxCompleteMbx();
-	if(m & can::mailbox::MBX0)
-	{
-	  CAN::cleanTxComplete<0>();
-	}
-	if(m & can::mailbox::MBX1)
-	{
-	  CAN::cleanTxComplete<1>();
-	}
-	if(m & can::mailbox::MBX2)
-	{
-	  CAN::cleanTxComplete<2>();
-	}
-	// Look for some external FIFO ?
-}
-
-void interrupt::CAN_RX0()
-{
-  ++rx0i;
-
-  while(CAN::msgRxPending<0>())
-  {
-    CAN::Receive<0>(&m0);
-    m0i = true;
-  }
-  if(CAN::rxFifoOverrun<0>()) {}
-  if(CAN::rxFifoFull<0>()) {}
-}
-
-void interrupt::CAN_RX1()
-{
-  ++rx1i;
-
-  while(CAN::msgRxPending<1>())
-  {
-    CAN::Receive<1>(&m1);
-    m1i = true;
-  }
-  if(CAN::rxFifoOverrun<1>()) {}
-  if(CAN::rxFifoFull<1>()) {}
-}
-
-void interrupt::CAN_SCE()
-{
-  ++erri;
-}
-
-/*
-void interrupt::USART1()
-{ TODO: Buffered TX and RX
-
-}
-*/
